7b1db3afbddc041efab833dd671b0c8ea7aba9aa
JustintheBrave <55635392+JustintheBrave@users.noreply.github.com>
2021-06-11 18:25:28 -0700

    robots

--- src/_00_Intro_To_Arrays/_02_RobotRace.java
+++ src/_00_Intro_To_Arrays/_02_RobotRace.java
@@ -8,13 +8,15 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class _02_RobotRace {
     // 1. make a main method
-
+		public static void main(String[] args) {
         // 2. create an array of 5 robots.
-
-        // 3. use a for loop to initialize the robots.
-
+			Robot rob[]=new Robot[5];
+		// 3. use a for loop to initialize the robots.
+			for (int i = 0; i < rob.length; i++) {
+				rob [i] = new Robot();
+			}
         // 4. make each robot start at the bottom of the screen, side by side, facing up
-    
+			
         // 5. use another for loop to iterate through the array and make each robot move
         // a random amount less than 50.
     
@@ -26,5 +28,6 @@ public class _02_RobotRace {
         // 8. try different races with different amounts of robots.
     
         // 9. make the robots race around a circular track.
+		}
 
 }
